412083c7e4677143b21515928d0624f7262453fd
Martin Fleischmann <martin@martinfleischmann.net>
2020-11-19 09:37:13 +0000

    morphom

--- nairobi test/utils.py
+++ nairobi test/utils.py
@@ -1,38 +1,23 @@
 import geopandas as gpd
 import pygeos
 import numpy as np
+from libpysal.weights import Queen
 
 
-def new_blocks(tessellation, edges, buildings, id_name="bID", unique_id="uID"):
-    """Generate tessellation-based blocks
-    """
-    blocks_single = (
-        gpd.overlay(
-            tessellation,
-            gpd.GeoDataFrame(geometry=edges.buffer(0.001)),
-            how="difference",
-        )
-        .dissolve(by=lambda x: 0)
-        .explode()
-        .drop(columns="uID")
-    )
-    blocks_single[id_name] = range(len(blocks_single))
+def _pysal_blocks(tessellation, edges, buildings, id_name='bID', unique_id='uID'):
+    cut = gpd.overlay(tessellation, gpd.GeoDataFrame(geometry=edges.buffer(0.001)), how='difference').explode()
+    W = Queen.from_dataframe(cut, silence_warnings=True)
+    cut['component'] = W.component_labels
     buildings_c = buildings.copy()
     buildings_c["geometry"] = buildings_c.representative_point()  # make points
     centroids_tempID = gpd.sjoin(
-        buildings_c, blocks_single, how="left", op="intersects"
+        buildings_c, cut[['geometry', 'component']], how="left", op="intersects"
     )
-
-    cells_copy = tessellation[[unique_id, "geometry"]].merge(
-        centroids_tempID[[unique_id, id_name]], on=unique_id, how="left"
-    )
-    blocks = cells_copy.dissolve(by=id_name).explode().reset_index(drop=True)
-
+    cells_copy = tessellation[[unique_id, "geometry"]].merge(centroids_tempID[[unique_id, 'component']], on='uID', how="left")
+    blocks = cells_copy.dissolve(by='component').explode().reset_index(drop=True)
     blocks[id_name] = range(len(blocks))
-    blocks["geometry"] = gpd.GeoSeries(
-        pygeos.polygons(blocks.exterior.values.data), crs=blocks.crs
-    )
-
+    blocks["geometry"] = gpd.GeoSeries(pygeos.polygons(blocks.exterior.values.data), crs=blocks.crs)
+    blocks = blocks[[id_name, 'geometry']]
     # if polygon is within another one, delete it
     inp, res = blocks.sindex.query_bulk(blocks.geometry, predicate="within")
     inp = inp[~(inp == res)]
@@ -40,16 +25,21 @@ def new_blocks(tessellation, edges, buildings, id_name="bID", unique_id="uID"):
     mask[inp] = False
     blocks = blocks.loc[mask, [id_name, "geometry"]]
 
-    centroids_w_bl_ID2 = gpd.sjoin(buildings_c, blocks, how="left", op="intersects")
+    centroids_w_bl_ID2 = gpd.sjoin(
+        buildings_c, blocks, how="left", op="intersects"
+    )
     bl_ID_to_uID = centroids_w_bl_ID2[[unique_id, id_name]]
 
-    buildings_m = buildings[[unique_id]].merge(bl_ID_to_uID, on=unique_id, how="left")
+    buildings_m = buildings[[unique_id]].merge(
+        bl_ID_to_uID, on=unique_id, how="left"
+    )
     buildings_id = buildings_m[id_name]
 
-    cells_m = tessellation[[unique_id]].merge(bl_ID_to_uID, on=unique_id, how="left")
+    cells_m = tessellation[[unique_id]].merge(
+        bl_ID_to_uID, on=unique_id, how="left"
+    )
     tessellation_id = cells_m[id_name]
     return (blocks, buildings_id, tessellation_id)
-
 
 def fill_insides(df):
     """
